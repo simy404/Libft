49e2e310803b0b16263975746d2c6bff7c2b0aab
simy404 <asimetra404@gmail.com>
2024-10-14 12:51:59 +0200

    mem variable type mark unsigned in ft_memcmp function

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -15,14 +15,14 @@
 int ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	size_t i;
-	char* s1_mem;
-	char* s2_mem;
+	unsigned char* s1_mem;
+	unsigned char* s2_mem;
 
 	if(!s1 || !s2 || n == 0)
 		return (0);
 	i = 0;
-	s1_mem = (char*)s1;
-	s2_mem = (char*)s2;
+	s1_mem = (unsigned char*)s1;
+	s2_mem = (unsigned char*)s2;
 
 	while(s1_mem[i] == s2_mem[i] && i < n - 1)
 	{
