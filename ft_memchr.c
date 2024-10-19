22569763969a755199d1eca8399fd76b644145e5
Hakan Can Samir <hsamir@k2m16s01.42kocaeli.com.tr>
2024-10-19 16:44:13 +0300

    fix

--- ft_memchr.c
+++ ft_memchr.c
@@ -14,26 +14,18 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t	i;
+	size_t			i;
 	unsigned char	*mem;
 
 	i = 0;
 	mem = (unsigned char *)s;
 	if (!s)
 		return (0);
-	while (mem[i] && i < n)
-	{	
+	while (i < n)
+	{
 		if (mem[i] == (unsigned char)c)
 			return ((void *)(s + i));
 		i++;
 	}
 	return (0);
-}
-
-#include "stdio.h"
-#include "string.h"
-int main()
-{
-	char s[] = {0, 1, 2 ,3 ,4 ,5};
-	printf("%s", (char*)memchr(s, 0 ,1));
 }
--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -14,8 +14,34 @@
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
-	while (*src && dstsize-- > 1)
-		*dst++ = *src++;
-	*dst = '\0';
-	return (ft_strlen(src));
+	// while (*src && dstsize-- > 1)
+	// 	*dst++ = *src++;
+	// *dst = '\0';
+	// return (ft_strlen(src));
+
+	size_t i;
+	size_t size;
+	if(!dst || !src)
+		return 0;
+	i = 0;
+	size = ft_strlen(dst);
+	while(size + i < dstsize - 1 && src[i]) // -> 5 + 2 < 7
+	{
+		dst[size + i] = src[i];
+		i++;
+	}
+	dst[size + i] = '\0';
+
+	return (size + i);
+
+}
+
+#include "string.h"
+#include "stdio.h"
+int main()
+{
+	char* src = "123456789";
+	char dest[13] = "12345";
+	size_t test = ft_strlcpy(dest, src, sizeof(dest));
+	printf("str : %s,test:  %zu ", dest, test);
 }
