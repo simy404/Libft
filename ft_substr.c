829dba615b70e1543044bd55046085c393504b29
simy404 <asimetra404@gmail.com>
2024-10-23 09:14:16 +0300

    fix substr

--- ft_substr.c
+++ ft_substr.c
@@ -12,32 +12,23 @@
 
 #include "libft.h"
 
-char *ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char *substr;
-	size_t i;
-	size_t s_len;
+	char	*substr;
+	size_t	s_len;
 
-	i = 0;
+	if (!s)
+		return (NULL);
 	s_len = ft_strlen(s);
 	if (start >= s_len)
 		return (ft_strdup(""));
-	if(len > s_len - start)
+	if (len > s_len - start)
 		len = s_len - start;
-	if (!s || !(substr = (char *)malloc(len + 1)))
+	substr = (char *)malloc(len + 1);
+	if (!substr)
 		return (0);
-
-    ft_memcpy(substr, s + start, len);
-    substr[len] = '\0';
+	ft_memcpy(substr, s + start, len);
+	substr[len] = '\0';
 	return (substr);
 }
 
-
-// #include <stdio.h>
-// int main()
-// {
-// 	char *s = "Hello World!";
-// 	printf("%s\n", ft_substr(s, 6, 5));
-// 	printf("%s\n", ft_substr3(s, 6, 5));
-// 	return 0;
-// }
