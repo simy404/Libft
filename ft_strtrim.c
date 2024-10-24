e26d707c7dbff3bfeab85a4da48837eb2f7159b5
simy404 <asimetra404@gmail.com>
2024-10-24 08:30:50 +0300

    update: strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -12,61 +12,43 @@
 
 #include "libft.h"
 
-int is_in_set(char c, char const *set)
+int	get_first_position(char const *str, char const *set)
 {
-	while (*set)
-	{
-		if (c == *set)
-			return (1);
-		set++;
-	}
-	return (0);
-}
-
-int get_first_position(char const *str, char const *set)
-{
-	int i;
+	int	i;
 
 	i = 0;
-	while(str[i] && is_in_set(str[i], set))
+	while (str[i] && ft_strchr(set, str[i]))
 		i++;
 	return (i);
 }
 
-int get_last_position(char const *str, char const *set)
+int	get_last_position(char const *str, char const *set)
 {
-	int i;
+	int	i;
 
 	i = ft_strlen(str) - 1;
-	while(str[i] && is_in_set(str[i], set))
+	while (str[i] && ft_strchr(set, str[i]))
 		i--;
 	return (i);
 }
 
-char *ft_strtrim(char const *s1, char const *set)
+char	*ft_strtrim(char const *s1, char const *set)
 {
-	char *str;
-	int start;
-	int end;
-	int len;
+	// char *str;
+	int	start;
+	int	end;
+	// int len;
 
 	if (!s1)
 		return (0);
 	start = get_first_position(s1, set);
 	end = get_last_position(s1, set);
 	if (start > end)
-		return (malloc(1));
-	len = end - start + 1;
-	str = (char *)malloc(len + 1);
-	if (!str)
-		return (0);
-	ft_strlcpy(str, s1 + start, len + 1);
-	return (str);
+		return (ft_strdup(""));
+	// len = end - start + 1;
+	// str = (char *)malloc(len + 1);
+	// if (!str)
+	// 	return (0);
+	// ft_strlcpy(str, s1 + start, len + 1);
+	return (ft_substr(s1, start, end - start + 1));
 }
-
-// // here the main function to test the function
-// #include <stdio.h>
-// int main()
-// {
-// 	printf("%s", ft_strtrim(" ,*+  lorem ipsum dolor sit amet   ", " ,*+"));
-// }
--- libft.h
+++ libft.h
@@ -36,6 +36,7 @@ size_t	ft_strlen(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strdup(const char *s);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
 
 
 #endif
